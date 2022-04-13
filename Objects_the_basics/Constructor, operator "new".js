e0c07b0530b31bc54a578a55e9c3a537e2655621
Anatoly Antsupov <NAnatolyAntsupovN@yandex.ru>
2022-04-14 01:45:15 +0300

    Objects

--- Objects_the_basics/Constructor, operator "new".js
+++ Objects_the_basics/Constructor, operator "new".js
@@ -25,7 +25,7 @@ console.log(user.isAdmin); // false
 // То есть, результат вызова функции это тот же объект, что и:
 let user1 = {
     name: "Vasya",
-    isAdmin = false
+    isAdmin: false
 };
 
 // Теперь, когда нам необходимо будет создать других пользователей, мы можем использовать
@@ -45,3 +45,119 @@ let user2 = new function() {
     this.name = "Vasya";
     this.isAdmin = false;
 }
+// Такой конструктор не может быть вызван дважды, так как он нигде не сохраняется, просто
+// создается и тут же вызывается. Таким образом, такой метод создания позволяет
+// инкапсулировать код, который создает отдельный объект, но без возможности его
+// повторного использования
+
+// Проверка на вызов в режиме конструктора: new.target
+// Данный метод используется очень редко
+
+// Возврат значения из конструктора return
+
+// Обычно конструкторы ничего не возвращают явно. Их задача - записать все необходимое в this, 
+// который в итоге станет результатом. 
+// Но если return все же есть, то применяется просто правило:
+// - при вызове return с объектом, будет возвращен объект, а не this
+// - при вызове return с примитивным значением, примитивное значение будет отброшено
+// Другими словами, return с объектом возвращает объект, в любом другом случае конструктор
+// вернет this.
+function BigUser() {
+    this.name = "Vasya";
+
+    return { name: "Godzilla" }; // возвращает этот объект (абстрактный какой-то)
+}
+
+console.log( new BigUser().name ); // Godzilla, получили этот объект
+
+// А вот пример с пустым (или мы могли поставить примитив после return, неважно)
+
+function SmallUser() {
+    this.name = "Vasya";
+
+    return; // возвращает this
+}
+
+console.log( new SmallUser().name ); // Vasya
+// Обычно у конструкторов отсутствует return. В данном блоке мы упомянули особое поведение с
+// возвращаемыми объектами, чтобы не оставлять пробелов в изучении языка 
+
+// Отсутствие скобок
+
+// Кстати, мы можем не ставить скобки после new, если вызов конструктора идет без аргументов
+let user3 = new User; // без скобок
+// то же, что и
+let user4 = new User();
+
+// Создание методов в конструкторе
+
+// Использование конструкторов для создания объектов дает большую гибкость. Можно передавать
+// конструктору параметры, определяющие, как создавать объект, и что в него записывать.
+// В this мы можем добавлять не только свойства, но и методы. 
+// Например, в примере ниже, new User(name) создает объект с данным именем name и методом sayHi
+function User1(name) {
+    this.name = name;
+
+    this.sayHi = function() {
+        console.log( `My name is ${name}` );
+    };
+}
+
+let vasya = new User1("Vasya");
+
+vasya.sayHi();
+// Для создания сложных объектов есть и более "продвинутый" синтаксис - классы, которые мы
+// разберем позже 
+
+/////////////////////////////////////////////
+///////////// Questions (2/3) ///////////////
+/////////////////////////////////////////////
+
+// Two functions - one object (!)
+
+let obj = {};
+
+function A() {
+    return obj;
+}
+
+function B() {
+    return obj;
+}
+
+let a = new A;
+let b = new B;
+
+console.log( a == b );
+// Да, возможно, неправильно ответил, что невозможно после одного неудачного теста
+
+// Create new Calculator
+/*function Calculator() {
+    this.read = function() {
+        this.theFirstValue = +prompt( "Enter the first value", "" );
+        this.theSecondValue = +prompt( "Enter the second value", "" );
+    },
+    this.sum = function() {
+        return(this.theFirstValue + this.theSecondValue);
+    },
+    this.mul = function() {
+        return(this.theFirstValue * this.theSecondValue);
+    }
+}
+
+let calculator = new Calculator;
+calculator.read();
+
+console.log( "Sum=" + calculator.sum() );
+console.log( "Mul=" + calculator.mul() );*/
+// Не надо забывать о правильном объявлении метода в конструкторе - this.method = function() {}
+
+// Create new Accumulator
+/*function Accumulator(startingValue) {
+    this.value = startingValue;
+
+    this.read = function() {
+        this.addValue = +prompt( "Enter value", "" );
+        this.value += this.addValue;
+    }
+}*/
