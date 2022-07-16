0aaa0843f65751079f5886403ca555495e0dac8a
Anatoly Antsupov <NAnatolyAntsupovN@yandex.ru>
2022-07-16 16:09:14 +0300

    tests

--- Data_types/11_Date_and_Time.js
+++ Data_types/11_Date_and_Time.js
@@ -129,3 +129,188 @@ date5.setDate(date5.getDate() + 2);
 
 console.log( date5 ); // 1 Mar 2016
 
+// Преобразование к числу, разность дат
+console.log(
+`\n----------------------------------------------
+|    Преобразование к числу, разность дат    |
+----------------------------------------------\n`);
+
+// Если объект Date преобразовать в число, то получим таймстамп по аналогии с date.getTime():
+let date6 = new Date();
+console.log( +date6 ); // 1657969545544
+
+// Также можно использовать вычитание дат для измерения времени: 
+let start = new Date(); // начинаем отсчёт времени
+
+// выполняем некоторые действия
+for (let i = 0; i < 100000; i++) {
+    let array = [];
+  
+    array.push(i);
+}
+
+let end = new Date(); // заканчиваем отсчёт времени
+
+console.log( `Цикл отработал за ${end - start} миллисекунд` );
+
+// Date.now()
+console.log(
+`\n--------------------
+|    Date.now()    |
+--------------------\n`);
+
+// Если нужно просто измерить время, объект Date нам не нужен.
+
+// Существует особый метод Date.now(), возвращающий текущую метку времени.
+// Семантически он эквивалентен new Date().getTime(), однако метод не создаёт промежуточный объект Date. 
+// Так что этот способ работает быстрее и не нагружает сборщик мусора.
+// Данный метод используется из соображений удобства или когда важно быстродействие.
+// Например, при разработке игр на JavaScript или других специализированных приложений
+
+// Предыдущий пример лучше переписать так:
+let start2 = Date.now(); // количество миллисекунд с 1 января 1970 года
+
+// выполняем некоторые действия
+for (let i = 0; i < 100000; i++) {
+    let array = [];
+  
+    array.push(i);
+}
+
+let end2 = Date.now(); // заканчиваем отсчёт времени
+
+console.log( `Цикл отработал за ${end2 - start2} миллисекунд` ); // вычитаются числа, а не даты
+
+// Бенчмаркинг
+console.log(
+`\n---------------------
+|    Бенчмаркинг    |
+---------------------\n`);
+
+// Надо быть внимательным при тестировании производительности функции, которая зависит от процессора.
+// Вычисления, замеряющие производительность, называются 'бенчмарками' (benchmark)
+
+let date7 = new Date(1980, 1, 15);
+let date8 = new Date(1981, 1, 15);
+
+function diffSubtract(date7, date8) {
+    return date8 - date7;
+  }
+  
+function diffGetTime(date7, date8) {
+    return date8.getTime() - date7.getTime();
+}
+  
+function bench(f) {
+    let start = Date.now();
+
+    for (let i = 0; i < 100000; i++) {
+        f(date7, date8);
+    }
+
+    return Date.now() - start;
+}
+
+let time1 = 0;
+let time2 = 0;
+
+// добавляем для 'разогрева' перед основным циклом
+bench(diffSubtract);
+bench(diffGetTime);
+
+// bench(diffSubtract) и bench(diffGetTime) поочерёдно запускаются 10 раз
+for (let i = 0; i < 10; i++) {
+    time1 += bench(diffSubtract);
+    time2 += bench(diffGetTime);
+}
+  
+console.log( 'Время diffSubtract: ' + bench(diffSubtract) + 'мс' );
+console.log( 'Время diffGetTime: ' + bench(diffGetTime) + 'мс' );
+  
+console.log( 'Итоговое время diffSubtract: ' + time1 );
+console.log( 'Итоговое время diffGetTime: ' + time2 );
+
+// Вот это да! Метод getTime() работает ощутимо быстрее! 
+// Всё потому, что не производится преобразование типов, и интерпретаторам такое намного легче оптимизировать.
+// Но для получения наиболее достоверных результатов тестирования производительности весь набор бенчмарков 
+// нужно запускать по нескольку раз.
+// Например: добавить к предыдущему примеру дополнительный цикл for. 
+// Нелишним будет добавить предварительный запуск для 'разогрева'
+
+// Разбор строки с датой
+console.log(
+`\n-------------------------------
+|    Разбор строки с датой    |
+-------------------------------\n`);
+
+// Бесполезная ерунда
+
+console.log(
+`\n----------------------
+|    Задачи (3/8)    |
+----------------------\n`);
+
+// Создайте дату
+
+let new_date = new Date(2012, 1, 21, 7, 12); // html работает с другими числами
+
+console.log( new_date );
+
+// Покажите день недели (!) - есть более красивый вариант
+
+function getWeekDay(date) {
+    if (date.getDay() == 0) {
+        return 'ВС';
+    }
+    if (date.getDay() == 1) {
+        return 'ПН';
+    }
+    if (date.getDay() == 2) {
+        return 'ВТ';
+    }
+    if (date.getDay() == 3) {
+        return 'СР';
+    }
+    if (date.getDay() == 4) {
+        return 'ЧТ';
+    }
+    if (date.getDay() == 5) {
+        return 'ПТ';
+    }
+    if (date.getDay() == 6) {
+        return 'СБ';
+    }
+}
+
+let example = new Date(2012, 0, 3);  // 3 января 2012 года
+
+console.log( getWeekDay(example) );   
+
+// День недели в европейской нумерации
+
+function getLocalDay(date) {
+    let days = ['7', '1', '2', '3', '4', '5', '6'];
+    
+    return +days[date.getDay()];
+}
+
+let example2 = new Date(2012, 0, 2);  // 3 января 2012 года
+
+console.log( getLocalDay(example2) );
+console.log( example2.getDay() );
+
+// Какой день месяца был много дней назад? (!) - есть немного иное решение, но в целом ок
+
+function getDateAgo(date, days) {
+    let difference = days * 24 * 3600 * 1000;
+    let time = date.getTime();
+    let answer = new Date(time - difference);
+
+    return answer.getDate();
+}
+
+let date9 = new Date(2015, 0, 2);
+
+console.log( getDateAgo(date9, 1) ); // 1, (1 Jan 2015)
+console.log( getDateAgo(date9, 2) ); // 31, (31 Dec 2014)
+console.log( getDateAgo(date9, 365) ); // 2, (2 Jan 2014)
